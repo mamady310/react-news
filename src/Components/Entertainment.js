313564371cec85b606cc290ee6aca128d4026d8f
Mamady Sillah <supurpisces310@msn.com>
2020-05-11 17:01:08 -0400

    added Entertainment.js

---
+++ src/Components/Entertainment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Entertainment extends Component {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Entertainment;
