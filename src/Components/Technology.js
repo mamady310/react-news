2c1625b7f020871110270ce9ccc9565389b7a603
Mamady Sillah <supurpisces310@msn.com>
2020-05-11 17:02:39 -0400

    completed Technology.js

---
+++ src/Components/Technology.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Technology extends Component {
+    render (){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+};
+
+export default Technology;
