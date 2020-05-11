3fef3fd04b5fbd8c53a303fc4a17b955558e8257
Mamady Sillah <supurpisces310@msn.com>
2020-05-11 17:12:37 -0400

    did componentDidMOunt and console.log(this.state.recipes) and returned an array. Now going to create a component for the recipe that returns 3 key value pairs from the array to be rendered on the main page

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import NavBar from '../Components/NavBar';
+import Main from '../Components/Main';
+import Entertainment from '../Components/Entertainment';
+import Technology from '../Components/Technology';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   
@@ -10,7 +14,17 @@ class App extends Component {
       recipes: [],
     }
   }
+
+  componentDidMount () {
+    axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => {
+      const meals = res.data.meals
+      this.setState({ recipes: meals})
+      console.log(this.state.recipes)
+    })
+  }
    render (){
+
      return (
        <div>
          <NavBar />
