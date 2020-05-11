54a77394917f4b58e6a5c73a50139b09acae1f48
Mamady Sillah <supurpisces310@msn.com>
2020-05-11 16:57:16 -0400

    added NavBar into div added additional Main, Entertainment, and Technology

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-
+import NavBar from '../Components/NavBar';
 import './App.css';
 
 class App extends Component {
    render (){
      return (
        <div>
+         <NavBar />
+         <Main />
+         <Entertainment />
+         <Technology />
 
        </div>
      )
